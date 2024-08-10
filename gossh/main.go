93df68c1b21d05404959a2540f984da9f6a3a141
huangrui <you@example.com>
2024-08-10 21:32:08 +0800

    修改路由

--- gossh/main.go
+++ gossh/main.go
@@ -46,97 +46,90 @@ func (w StaticFile) Open(name string) (fs.File, error) {
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	var engine = gin.Default()
-	engine.Use(middleware.DbCheck())
-	engine.Use(middleware.NetFilter())
+	engine.Use(middleware.DbCheck(), middleware.NetFilter())
 
 	engine.NoRoute(func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/app")
 	})
 
-	engine.POST("/api/login", service.UserLogin)
-	engine.POST("/api/sys/db_conn_check", service.DbConnCheck)
-	engine.GET("/api/sys/is_init", service.GetIsInit)
-	engine.POST("/api/sys/init", service.SysInit)
+	// 不需要认证的路由
+	var open = engine.Group("")
+	open.StaticFS("/app", http.FS(StaticFile{embedFS: dir, path: "webroot"}))
+	open.POST("/api/login", service.UserLogin)
+	open.POST("/api/sys/db_conn_check", service.DbConnCheck)
+	open.GET("/api/sys/is_init", service.GetIsInit)
+	open.POST("/api/sys/init", service.SysInit)
 
-	var router = engine.Group("",
-		middleware.SysInit(),
-		middleware.JWTAuth(),
-	)
+	// 需要认证的路由
+	var auth = engine.Group("", middleware.SysInit(), middleware.JWTAuth())
 
 	{ // SSH 连接配置
-		router.GET("/api/conn_conf", service.ConfFindAll)
-		router.GET("/api/conn_conf/:id", service.ConfFindByID)
-		router.POST("/api/conn_conf", service.ConfCreate)
-		router.PUT("/api/conn_conf", service.ConfUpdateById)
-		router.DELETE("/api/conn_conf/:id", service.ConfDeleteById)
+		auth.GET("/api/conn_conf", service.ConfFindAll)
+		auth.GET("/api/conn_conf/:id", service.ConfFindByID)
+		auth.POST("/api/conn_conf", service.ConfCreate)
+		auth.PUT("/api/conn_conf", service.ConfUpdateById)
+		auth.DELETE("/api/conn_conf/:id", service.ConfDeleteById)
 	}
 
 	{ // 命令收藏
-		router.GET("/api/cmd_note", service.CmdNoteFindAll)
-		router.GET("/api/cmd_note/:id", service.CmdNoteFindByID)
-		router.POST("/api/cmd_note", service.CmdNoteCreate)
-		router.PUT("/api/cmd_note", service.CmdNoteUpdateById)
-		router.DELETE("/api/cmd_note/:id", service.CmdNoteDeleteById)
+		auth.GET("/api/cmd_note", service.CmdNoteFindAll)
+		auth.GET("/api/cmd_note/:id", service.CmdNoteFindByID)
+		auth.POST("/api/cmd_note", service.CmdNoteCreate)
+		auth.PUT("/api/cmd_note", service.CmdNoteUpdateById)
+		auth.DELETE("/api/cmd_note/:id", service.CmdNoteDeleteById)
 	}
 
 	{ // 策略配置
-		router.GET("/api/policy_conf", service.PolicyConfFindAll)
-		router.GET("/api/policy_conf/:id", service.PolicyConfFindByID)
-		router.POST("/api/policy_conf", service.PolicyConfCreate)
-		router.PUT("/api/policy_conf", service.PolicyConfUpdateById)
-		router.DELETE("/api/policy_conf/:id", service.PolicyConfDeleteById)
+		auth.GET("/api/policy_conf", service.PolicyConfFindAll)
+		auth.GET("/api/policy_conf/:id", service.PolicyConfFindByID)
+		auth.POST("/api/policy_conf", service.PolicyConfCreate)
+		auth.PUT("/api/policy_conf", service.PolicyConfUpdateById)
+		auth.DELETE("/api/policy_conf/:id", service.PolicyConfDeleteById)
 	}
 
 	{ // 访问控制
-		router.GET("/api/net_filter", service.NetFilterFindAll)
-		router.GET("/api/net_filter/:id", service.NetFilterFindByID)
-		router.POST("/api/net_filter", service.NetFilterCreate)
-		router.PUT("/api/net_filter", service.NetFilterUpdateById)
-		router.DELETE("/api/net_filter/:id", service.NetFilterDeleteById)
+		auth.GET("/api/net_filter", service.NetFilterFindAll)
+		auth.GET("/api/net_filter/:id", service.NetFilterFindByID)
+		auth.POST("/api/net_filter", service.NetFilterCreate)
+		auth.PUT("/api/net_filter", service.NetFilterUpdateById)
+		auth.DELETE("/api/net_filter/:id", service.NetFilterDeleteById)
 	}
 
 	{ // 用户管理
-		router.GET("/api/user", service.UserFindAll)
-		router.GET("/api/user/:id", service.UserFindByID)
-		router.POST("/api/user", service.UserCreate)
-		router.PUT("/api/user", service.UserUpdateById)
-		router.DELETE("/api/user/:id", service.UserDeleteById)
-		router.PATCH("/api/user/check_name_exists", service.CheckUserNameExists)
-		router.PATCH("/api/user/pwd", service.ModifyPasswd)
+		auth.GET("/api/user", service.UserFindAll)
+		auth.GET("/api/user/:id", service.UserFindByID)
+		auth.POST("/api/user", service.UserCreate)
+		auth.PUT("/api/user", service.UserUpdateById)
+		auth.DELETE("/api/user/:id", service.UserDeleteById)
+		auth.PATCH("/api/user/check_name_exists", service.CheckUserNameExists)
+		auth.PATCH("/api/user/pwd", service.ModifyPasswd)
 	}
 
 	{ // 审计日志
-		router.POST("/api/login_audit", service.LoginAuditSearch)
+		auth.POST("/api/login_audit", service.LoginAuditSearch)
 	}
 
 	{ // SSH链接
-		router.GET("/api/conn_manage/online_client", service.GetOnlineClient)
-		router.PUT("/api/conn_manage/refresh_conn_time", service.RefreshConnTime)
-		router.POST("/api/sftp/create_dir", service.SftpCreateDir)
-		router.POST("/api/sftp/list", service.SftpList)
-		router.GET("/api/sftp/download", service.SftpDownLoad)
-		router.PUT("/api/sftp/upload", service.SftpUpload)
-		router.DELETE("/api/sftp/delete", service.SftpDelete)
-		router.GET("/api/ssh/conn", service.NewSshConn)
-		router.PATCH("/api/ssh/conn", service.ResizeWindow)
-		router.POST("/api/ssh/exec", service.ExecCommand)
-		router.POST("/api/ssh/disconnect", service.Disconnect)
-		router.POST("/api/ssh/create_session", service.CreateSessionId)
+		auth.GET("/api/conn_manage/online_client", service.GetOnlineClient)
+		auth.PUT("/api/conn_manage/refresh_conn_time", service.RefreshConnTime)
+		auth.POST("/api/sftp/create_dir", service.SftpCreateDir)
+		auth.POST("/api/sftp/list", service.SftpList)
+		auth.GET("/api/sftp/download", service.SftpDownLoad)
+		auth.PUT("/api/sftp/upload", service.SftpUpload)
+		auth.DELETE("/api/sftp/delete", service.SftpDelete)
+		auth.GET("/api/ssh/conn", service.NewSshConn)
+		auth.PATCH("/api/ssh/conn", service.ResizeWindow)
+		auth.POST("/api/ssh/exec", service.ExecCommand)
+		auth.POST("/api/ssh/disconnect", service.Disconnect)
+		auth.POST("/api/ssh/create_session", service.CreateSessionId)
 	}
 
 	{ // 系统配置
-		router.GET("/api/sys/config", service.GetRunConf)
-		router.POST("/api/sys/config", service.SetRunConf)
+		auth.GET("/api/sys/config", service.GetRunConf)
+		auth.POST("/api/sys/config", service.SetRunConf)
 	}
 
-	// 处理前端静态文件
-	engine.StaticFS("/app", http.FS(StaticFile{
-		embedFS: dir,
-		path:    "webroot",
-	}))
-
 	address := fmt.Sprintf("%s:%s", config.DefaultConfig.Address, config.DefaultConfig.Port)
-
 	_, certErr := os.Open(config.DefaultConfig.CertFile)
 	_, keyErr := os.Open(config.DefaultConfig.KeyFile)
 
